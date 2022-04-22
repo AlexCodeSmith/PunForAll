a009eb7103b4a0eecf3327990b0eeec73884d10c
AlexCodeSmith <smith.j.alex@hotmail.co.uk>
2022-04-22 10:40:59 +0100

    Update PunBot.py

--- PunBot.py
+++ PunBot.py
@@ -1,23 +1,34 @@
 import discord
 import random
 
-def checkPun(m,f):
+def getPuns(f):
+    plist=[]
+    puns = []
+    c = 0
+    with open(f) as file:
+        for line in file:
+            line = line.strip() #get rid of newline
+            plist.append(line.split(';')) #split the pun type from list and add it to plist
+            split = plist[c][1].split(',') #split the comma separated puns 
+            plist[c].remove(plist[c][1]) #remove them from plist
+            plist[c] += split #join plist with the now split puns
+            puns.append(plist[c][0]) #add the pun type to puns 
+            c+=1 #increment c                    
+    return plist,puns
 
-    with open(f,"r") as file:
-        puns = file.readlines().split()
-
+def checkPun(m,puns):
     for p in puns:
-        if p in m:
-            return p
-
+        if p.lower() in m.lower(): #check if the pun word is in the message
+             return p
     return False
 
-def pickPun(pun):
-    f = pun +".txt"
-    with open(f, "r") as file:
-        puns = file.readlines().split(",")
-    
-    return puns[random.randint(0,len(puns)-1)]
+
+def pickPun(plist,p):
+    for puns in plist:
+        if puns[0] == p:
+            return puns[random.randint(1,len(puns[0]))]
+
+plist, puns = getPuns("punlist.txt")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,15 +37,15 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Its fine now, why? For I am here! {client.user}')
+    print('Its fine now, why? For puns are here!')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    pun = checkPun(message.content,"punlist.txt")
-    if pun != False:
+    pun = checkPun(message.content,'punlist.txt')
+    if pun != False and random.randint(0,3) == 3:
         await message.channel.send(pickPun(pun))
 
 
